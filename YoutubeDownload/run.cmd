0e7716fe5f11d0114aada7a8ccdd622f12bc0985
watchakorn-18k <porton2559@gmail.com>
2021-09-12 10:44:31 +0700

    Update

--- YoutubeDownload/run.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-cmd /k "cd /d \wk-18k\Scripts & activate & cd /d    D:\Python_download_YT_MP3\YoutubeDownload & python main.py"
--- main.py
+++ main.py
@@ -4,35 +4,39 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 # i need GridLayout
-from kivy.properties import StringProperty,BooleanProperty
+from kivy.properties import StringProperty,BooleanProperty,NumericProperty
 from kivy.uix.gridlayout import GridLayout
-import webbrowser
 from kivy.uix.widget import Widget
+from kivy.uix.progressbar import ProgressBar
 import youtube_dl
 import os
 from kivy.core.window import Window
-
+from kivy.animation import Animation
+import time
 
 class gridlayout_Screen(GridLayout):
-    success_text = StringProperty("")
+    check_input = BooleanProperty(False)
     my_text = StringProperty("")
-    line_yt_text = StringProperty("")
-    check_offline_text = BooleanProperty(True)
+    success_download = StringProperty("")
+    process_download = StringProperty("")
+    count = 0
+    process_download_value = NumericProperty(0)
     def on_button_click(self):
+        self.ids.my_button_1.disabled = True
+        print(self.ids.my_button_1.disabled)
         if os.path.isdir("Download MP3"):
             PATH = '/Download MP3/'
         elif os.path.isdir("Download MP3") != True:
             os.mkdir("Download MP3")
             PATH = '/Download MP3/'
-
-
         try:
+            
             link = self.ids.my_text_input.text
             ydl_opts = {
                 'format':'worstaudio',
                 'extractaudio':True,
                 'audioformat':'mp3',
-                'outtmpl': PATH +u'%(title)s - %(artist)s.mp3',    #name the file the ID of the video
+                'outtmpl': PATH +u'%(title)s - 18k.mp3',    #name the file the ID of the video
                 'noplaylist':True,
                 'nocheckcertificate':True,
                 'postprocessors': [{
@@ -46,17 +50,44 @@ class gridlayout_Screen(GridLayout):
                 info_dict = ydl.extract_info(link, download=False)
                 video_title = info_dict.get('title', None)
                 video_duration = info_dict.get('duration', None)/60
-                print(video_title)
-                self.my_text = f"{video_title} - {int(video_duration)} นาที ดาวน์โหลดสำเร็จสมบูรณ์ "
+                self.my_text = f"{video_title}"
+                import re
+                def listToString(s):
+                    str1 = " "
+                    for ele in s: 
+                        str1 += ele
+                    return str1
+                self.my_text = f"{listToString(re.split(' ',self.my_text[0:30]))}..... - {int(video_duration)} นาที"
                 if self.my_text != "":
                     ydl.download([link])
-                    self.success_text = StringProperty("Success")
+                    
+        except :
+            print("-----------")
+            print(self.ids.my_text_input.text.find("youtube.com"))
+            if self.ids.my_text_input.text.find("youtube.com") == -1 :
+                self.process_download = " "
+            elif self.ids.my_text_input.text.find("youtube.com") >= 0 :
+                self.process_download = "ดาวน์โหลดเสร็จแล้ว"
+            self.ids.my_text_input.text = ""
+            self.ids.my_progress_bar.value = 0
+        for i in range(0,101):
+            self.ids.my_progress_bar.value = i
+      
+    def on_open_dir_mp3(self):
+        import os
+        path = "Download MP3"
+        path = os.path.realpath(path)
+        os.startfile(path)
+    def animate_it(self):
+        
+        animate = Animation(opacity=0,duration=0.1)
+        animate += Animation(opacity=1,)
+        animate.start(self.ids.btn_1)
 
-                
-                
-            
-        except:
-            print("Error")
+        
+        
+    
+    
 
 
 class YoutubeDownloadApp(App):
@@ -64,7 +95,11 @@ class YoutubeDownloadApp(App):
         self.title = 'ดาวน์โหลดเพลงจากยูทูป'
         self.icon = 'logo.png'
         self.title_color = 1,0,0,1
-        Window.size = (500, 600)
+        Window.size = (400, 600)
+
+
+
+
         
 
        
