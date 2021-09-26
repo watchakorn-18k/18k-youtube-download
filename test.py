752ecc30343bce89f1d36f841491f4f01751b709
watchakorn-18k <porton2559@gmail.com>
2021-09-26 11:51:02 +0700

    Updated 3.2.1.9

--- test.py
+++ test.py
@@ -1,6 +1,32 @@
-text = "https://music.youtube.com/watch?v=f02mOEt11OQ&list=RDAMVMf02mOEt11OQ"
+# import re
+# string_to_split = "[ re - ]  ／『もう一度』  18K.mp3"
 
-if text.find("https://music.youtube.com/") == 0:
-    t1 = text.split("https://music.youtube.com/")[1]
-    text_res = "https://youtube.com/"+t1
-
+# res = re.split('[^a-zA-Zก-ฮ0-9.ะาิืี๊่้ึ ุูแโเะั ำไใฤฤาฦฦา่้๊๋ๆ-]', string_to_split)
+# res = ''.join(res)
+# res = re.split('\s+', res)
+# res = ''.join(res)
+# print(res)
+# word = []
+# for i in res:
+#     if i != '':
+#         word.append(i)
+#     elif i == '':
+#         i = '_'
+#         word.append(i)
+        
+# word = ' '.join(word)
+# print(word)
+def split_text(text):
+    import re
+    string_to_split = text
+    res = re.split('[^a-zA-Zก-ฮ0-9.ะาิืี๊่้ึ ุูแโเะั ำไใฤฤาฦฦา่้๊๋ๆ-]', string_to_split)
+    res = ''.join(res)
+    res = re.split('\s+', res)
+    res = ''.join(res)
+    return res
+import os
+dir = "cache_MP3"
+for count, filename in enumerate(os.listdir(dir)):
+    dst = os.path.join(dir, f"{split_text(filename)}")
+    src = os.path.join(dir, filename)
+    os.rename(src, dst)
