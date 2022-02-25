882a5c01231c5aafac568e534d3873dcae1b92e8
watchakorn-18k <porton2559@gmail.com>
2022-02-25 12:13:09 +0700

    Update Add Check Short Cut to desktop page

---
+++ Assets/CheckShortCut.py
@@ -0,0 +1,19 @@
+import os,winshell,sys,ctypes
+
+desktop = winshell.desktop()
+files = os.listdir(desktop)
+print()
+# exit()
+def AlertCreateShortCut(title, text, style):
+    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+
+def CheckShortCut():
+  if "18k-youtube-download.lnk" not in files:
+      CheckYesNo = AlertCreateShortCut('สร้างทางลัดหน้าเดสก์ท็อป', 'คุณต้องสร้างทางลัดโปรแกรมไปหน้าเดสก์ท็อปหรือไม่',4)
+      if CheckYesNo == 6:
+        winshell.CreateShortcut(
+  Path=os.path.join(winshell.desktop(), "18k-youtube-download.lnk"),
+  Target=f"{os.getcwd()}\\18k-youtube-download.exe",
+  Icon=(f"{os.getcwd()}\\18k-youtube-download.exe", 0),
+  Description="18k youtube download"
+  )
